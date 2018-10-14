4f4e15326f1859dd9fdab88ac03894e640caa9b1
JaniKoivisto <koivisto.jani@live.fi>
2018-10-14 16:18:13 +0300

    Make decoding more generic

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import osmService as service
+import utils
 from flask_cors import CORS
-import base64
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -20,11 +20,8 @@ def loginOSM():
     decoded_username = ''
     decoded_password = ''
     if request.headers.get('Authorization'):
-        method, auth = request.headers.get('Authorization').split(' ')
-        if method.lower() == 'basic':
-            username, password = auth.split(':')
-            decoded_username = base64.b64decode(username).decode('utf-8')
-            decoded_password = base64.b64decode(password).decode('utf-8')
+        decode_username, decoded_password = utils.authDecoder(request)
+        
     response = service.loginOSM(decoded_username, decoded_password)
     return response
 
@@ -34,12 +31,8 @@ def addNode():
     decoded_password = ''
     
     if request.headers.get('Authorization'):
-        method, auth = request.headers.get('Authorization').split(' ')
-        if method.lower() == 'basic':
-            username, password = auth.split(':')
-            decoded_username = base64.b64decode(username).decode('utf-8')
-            decoded_password = base64.b64decode(password).decode('utf-8')
-    
+        decode_username, decoded_password = utils.authDecoder(request)
+
     changeset_id = request.args.get('changesetid')
     if changeset_id is None:
         resp_text, resp_code = service.addChangeset(decoded_username, decoded_password)
