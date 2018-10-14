be7d380e3faf4aa7fc43d36294bd0d52ccbacf6b
JaniKoivisto <koivisto.jani@live.fi>
2018-10-14 15:16:15 +0300

    Backend for adding an entrance node

--- backend/app.py
+++ backend/app.py
@@ -28,5 +28,38 @@ def loginOSM():
     response = service.loginOSM(decoded_username, decoded_password)
     return response
 
+@app.route("/api/node", methods=['POST', 'OPTIONS'])
+def addNode():
+    decoded_username = ''
+    decoded_password = ''
+    
+    if request.headers.get('Authorization'):
+        method, auth = request.headers.get('Authorization').split(' ')
+        if method.lower() == 'basic':
+            username, password = auth.split(':')
+            decoded_username = base64.b64decode(username).decode('utf-8')
+            decoded_password = base64.b64decode(password).decode('utf-8')
+
+    changeset_id = request.args.get('changesetid')
+    lon = request.args.get('lon')
+    lat = request.args.get('lat')
+
+    entrance_attributes = ['yes', 'main', 'service', 'exit', 'home']
+    entrance_attributes_feature = {}
+    for attribute in entrance_attributes:
+        if attribute in request.args:
+            entrance_attributes_feature[attribute] = request.args.get(attribute)
+    
+    entrance_address_attributes = ['housenumber', 'housename', 'flats', 'conscriptionnumber', 'street', 'place', 'postcode',
+                                    'city', 'country', 'full']
+
+    entrance_address_attributes_feature = {}
+    for address_attribute in entrance_address_attributes:
+        if address_attribute in request.args:
+            entrance_address_attributes_feature[address_attribute] = request.args.get(address_attribute)
+
+    response = service.addNode(changeset_id, lon, lat, entrance_attributes_feature, entrance_address_attributes_feature, decoded_username, decoded_password)
+    return response
+
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/osmService.py
+++ backend/osmService.py
@@ -1,5 +1,16 @@
 from flask import jsonify
+import xml.etree.cElementTree as ET
 import requests
+
+def makeNode(changeset_id, lon, lat, entrance_attributes, entrance_address_attributes):
+    osm = ET.Element("osm")
+    node = ET.SubElement(osm, "node", changeset=changeset_id, lat=lat, lon=lon)
+    for key, value in entrance_attributes.items():
+        ET.SubElement(node, "tag", k=key, v=value)
+    for key, value in entrance_address_attributes.items():
+        ET.SubElement(node, "tag", k="addr:" + key, v=value)
+    xml = ET.dump(osm)
+    return xml
 
 def addNote(params):
     response = requests.post('https://api.openstreetmap.org/api/0.6/notes', data=params)
@@ -7,4 +18,10 @@ def addNote(params):
 
 def loginOSM(username, password):
     response = requests.get('https://api.openstreetmap.org/api/0.6/user/details', auth=(username, password))
+    return jsonify({"message" : response.text, "code" : response.status_code})
+
+def addNode(changeset_id, lon, lat, entrance_attributes, entrance_address_attributes, username, password):
+    headers = {'Content-Type': 'application/xml'}
+    xml = makeNode(changeset_id, lon, lat, entrance_attributes, entrance_address_attributes)
+    response = requests.put('https://api.openstreetmap.org/api/0.6/node/create', data=xml, headers=headers, auth=(str(username), str(password)))
     return jsonify({"message" : response.text, "code" : response.status_code})
