458f7b6b677cdcd0aaa7f0f5593a263e854ad8da
JaniKoivisto <koivisto.jani@live.fi>
2018-12-29 19:09:28 +0200

    Frontend fetch for buildings, snapping interaction WIP, some cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-// import './App.css';
 import Navbar from './Navbar';
 import Map from './MapEmbed';
 import Modal from './Modal';
-import $ from 'jquery';
 
 class App extends Component {
   constructor(props) {
@@ -17,48 +15,41 @@ class App extends Component {
       password:'',
       response: false,
       entranceType: ''
-      };
-
-      this.toggleModal = this.toggleModal.bind(this);
-      this.osmLogin = this.osmLogin.bind(this);
-      this.handleLoginChange = this.handleLoginChange.bind(this);
-      this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
-      this.createNode = this.createNode.bind(this);
-      this.handleEntranceChange = this.handleEntranceChange.bind(this);
+    };
+    
+    this.toggleModal = this.toggleModal.bind(this);
+    this.osmLogin = this.osmLogin.bind(this);
+    this.handleLoginChange = this.handleLoginChange.bind(this);
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.createNode = this.createNode.bind(this);
+    this.handleEntranceChange = this.handleEntranceChange.bind(this);
   }
-
+  
   componentDidMount() {
     this.setState({
       isOpen: !this.state.isOpen,
     });
-
   };
-
+  
   toggleModal(event) {
-
     this.setState({
       isOpen: !this.state.isOpen,
     });
-
-
     event.preventDefault();
   };
-
+  
   osmLogin(event) {
     this.setState({
       osmUser: !this.state.osmUser,
     });
-
+    
   };
-
-
-
+  
   handleLoginChange(event) {
-      this.setState({ [event.target.name]: event.target.value });
+    this.setState({ [event.target.name]: event.target.value });
   };
-
+  
   handleLoginSubmit(event) {
-    console.log("login");
     const encodedUsername = new Buffer(this.state.username).toString('base64');
     const encodedPassword = new Buffer(this.state.password).toString('base64');
     let headers = new Headers();
@@ -67,41 +58,40 @@ class App extends Component {
       const response = await fetch(process.env.REACT_APP_API_URL + "/api/login"
       ,
       { 
-          method: 'GET',
-          headers: headers
+        method: 'GET',
+        headers: headers
       });
-
+      
       const json = await response.json();
       const code = json.code;
-
+      
       if (code === 200) {
         this.setState({
           isOpen: !this.state.isOpen,
         });
-
+        
       }
-
+      
     };
-
+    
     login();
     
-    //Change for successfull response
     this.setState({
-          response: true,
+      response: true,
     });
-
+    
     event.preventDefault();
-
+    
   };
-
+  
   updateLongitude = (entranceLongitude) => {this.setState({ entranceLongitude })};
-
+  
   updateLatitude = (entranceLatitude) => {this.setState({ entranceLatitude })};
-
+  
   handleEntranceChange(event) {
     this.setState({ entranceType: event.target.value });
   };
-
+  
   createNode() {
     const encodedUsername = new Buffer(this.state.username).toString('base64');
     const encodedPassword = new Buffer(this.state.password).toString('base64');
@@ -110,35 +100,35 @@ class App extends Component {
     headers.set('content-type', 'application/json');
     
     const addNode = async () => {
-        const response = await fetch(process.env.REACT_APP_API_URL + '/api/node?lat=' + this.state.entranceLatitude + '&lon=' + this.state.entranceLongitude + '&entrance=' + this.state.entranceType
-        ,
-        { 
-            method: 'POST',
-            headers: headers
-        });
-        
-        const json = await response.json();
-        //TODO: save changesetid
-        const nodeId = json.message;
-        const changesetId = json.changesetid;
-        console.log("nodeId: " + nodeId + " changeSet:" + changesetId);
-
+      const response = await fetch(process.env.REACT_APP_API_URL + '/api/node?lat=' + this.state.entranceLatitude + '&lon=' + this.state.entranceLongitude + '&entrance=' + this.state.entranceType
+      ,
+      { 
+        method: 'POST',
+        headers: headers
+      });
+      
+      const json = await response.json();
+      //TODO: save changesetid
+      const nodeId = json.message;
+      const changesetId = json.changesetid;
+      console.log("nodeId: " + nodeId + " changeSet:" + changesetId);
+      
     };
     
     addNode();
-
+    
   };
-
-
+  
   render() {
-      return (
-        <div className="App">        
-          <Navbar osmUser= {this.state.osmUser} lon={this.state.entranceLongitude} lat={this.state.entranceLatitude} createNode={this.createNode} handleChange={this.handleEntranceChange} entranceType={this.state.entranceType} />
-          <Map updateLongitude = {this.updateLongitude} updateLatitude = {this.updateLatitude} />
-          <Modal show={this.state.isOpen} onClose={this.toggleModal} onLogin={this.osmLogin} osmUser={this.state.osmUser} username={this.state.username} password={this.state.password} handleLoginChange={this.handleLoginChange} handleLoginSubmit={this.handleLoginSubmit} /> 
-        </div>
+    return (
+      <div className="App">        
+      <Navbar osmUser= {this.state.osmUser} lon={this.state.entranceLongitude} lat={this.state.entranceLatitude} createNode={this.createNode} handleChange={this.handleEntranceChange} entranceType={this.state.entranceType} />
+      <Map updateLongitude = {this.updateLongitude} updateLatitude = {this.updateLatitude} />
+      <Modal show={this.state.isOpen} onClose={this.toggleModal} onLogin={this.osmLogin} osmUser={this.state.osmUser} username={this.state.username} password={this.state.password} handleLoginChange={this.handleLoginChange} handleLoginSubmit={this.handleLoginSubmit} /> 
+      </div>
       );
+    }
   }
-}
-
-export default App;
+  
+  export default App;
+  
--- src/MapEmbed.js
+++ src/MapEmbed.js
@@ -3,7 +3,8 @@ import 'ol/ol.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import { fromLonLat, toLonLat } from 'ol/proj';
+import OSMXML from 'ol/format/OSMXML.js';
+import {fromLonLat,toLonLat} from 'ol/proj';
 import VectorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
 import {defaults as defaultControls, ScaleLine} from 'ol/control.js';
@@ -11,8 +12,10 @@ import Point from 'ol/geom/Point';
 import Feature from 'ol/Feature';
 import {Icon, Style} from 'ol/style.js';
 import icon from './pin.png';
-import { getCenter } from 'ol/extent';
 import Geolocation from 'ol/Geolocation.js';
+import {transformExtent} from 'ol/proj.js';
+import {bbox as bboxStrategy} from 'ol/loadingstrategy.js';
+import {Draw, Snap} from 'ol/interaction.js';
 
 class MapEmbed extends React.Component {
 	constructor() {
@@ -22,10 +25,11 @@ class MapEmbed extends React.Component {
 			entranceSource: null,
 			entranceLayer: null,
 			entranceMarker: null,
-			geolocation: null
+			geolocation: null,
+			buildings: null,
+			buildingSource: null
 		};
 	}
-	
 	
 	componentDidMount() {
 		var entranceMarker = new Feature({
@@ -37,7 +41,7 @@ class MapEmbed extends React.Component {
 				anchor: [0.5, 32],
 				anchorXUnits: 'fraction',
 				anchorYUnits: 'pixels',
-				src: icon//openlayers.org/en/v3.8.2/examples/data/icon.png'
+				src: icon
 			}))
 		});
 		
@@ -52,8 +56,31 @@ class MapEmbed extends React.Component {
 			source: entranceSource
 		});
 		
-		var scaleLineControl = new ScaleLine();
+		var buildingSource = new VectorSource({
+			format: new OSMXML(),
+			loader: function(extent, resolution, projection) {
+				var epsg4326Extent = transformExtent(extent, projection, 'EPSG:4326');
+				
+				var client = new XMLHttpRequest();
+				
+				client.open('GET', process.env.REACT_APP_API_URL + '/api/osmData?x=' + epsg4326Extent[1] + '&y=' + epsg4326Extent[0] +
+							'&xx=' + epsg4326Extent[3] + '&yy=' + epsg4326Extent[2]);
+				client.addEventListener('load', function() {
+					var features = new OSMXML().readFeatures(client.responseText, {
+						featureProjection: map.getView().getProjection()
+					});
+					buildingSource.addFeatures(features);
+				});
+				client.send();
+			},
+			strategy: bboxStrategy
+		});
+
+		var buildings = new VectorLayer({
+			source: buildingSource
+		});
 		
+		var scaleLineControl = new ScaleLine();
 		
 		const map = new Map({
 			target: this.refs.mapContainer,
@@ -61,6 +88,7 @@ class MapEmbed extends React.Component {
 				new TileLayer({
 					source: new OSM()
 				}),
+				buildings,
 				entranceLayer
 			],
 			
@@ -77,73 +105,79 @@ class MapEmbed extends React.Component {
 				zoom: 18
 			})
 		});
+		
+		var EntranceDraw = {
+			init: function() {
+				map.addInteraction(this.Point);
+				this.Point.setActive(true);
+			},
+			Point: new Draw({
+				source: buildingSource,
+				type: 'Point',
+				//style: this.state.iconStyle
+			})
+		};
 
-	  var geolocation = new Geolocation({
-      // enableHighAccuracy must be set to true to have the heading value.
-      trackingOptions: {
-        enableHighAccuracy: true
-      },
-      projection: map.getView().getProjection()
-    });
+		EntranceDraw.init();
 
-    geolocation.setTracking(true);
+		var EntranceSnap = new Snap({
+			source: buildings.getSource()
+		});
+
+		map.addInteraction(EntranceSnap);
+
+		var geolocation = new Geolocation({
+			// enableHighAccuracy must be set to true to have the heading value.
+			trackingOptions: {
+				enableHighAccuracy: true
+			},
+			projection: map.getView().getProjection()
+		});
+		
+		geolocation.setTracking(true);
 		
 		this.setState({ 
 			map: map,
 			entranceSource: entranceSource,
 			entranceLayer: entranceLayer,
 			entranceMarker: entranceMarker,
-			geolocation: geolocation
+			geolocation: geolocation,
+			buildings: buildings,
+			buildingSource: buildingSource
 		});
 		
-		//detect center change
 		map.on('pointerdrag', this.handleMapPan.bind(this));
 		map.on('postrender', this.handleMapPan.bind(this));
-
-		geolocation.on('change:position', this.handleGeolocation.bind(this));
 		
-		//end of didMount
+		geolocation.on('change:position', this.handleGeolocation.bind(this));
+
 	}
 	
 	
 	handleMapPan(coordinate) {
-		
-		//var extent = this.state.map.getView().calculateExtent(this.state.map.getSize());
 		var entranceCoordinates = this.state.map.getView().getCenter();
-		//console.log(entranceCoordinates);
 		this.state.entranceMarker.getGeometry().setCoordinates(entranceCoordinates);
-		
-		
-		// derive map coordinate (references map from Wrapper Component state)
+
 		var lon = toLonLat(entranceCoordinates)[0];
 		this.props.updateLongitude(lon);
 		
 		var lat = toLonLat(entranceCoordinates)[1];
 		this.props.updateLatitude(lat);
-		
 	}
-
+	
 	handleGeolocation(geolocation) {
 		var userPosition = this.state.geolocation.getPosition()
 		// disable tracking option after initial load
 		this.state.geolocation.setTracking(false);
-		// var newUserPosition = new Feature({
-	 //    geometry: new Point( userPosition ),
-	 //  });
-
-		//this.state.entranceSource.addFeature(newUserPosition);
 		this.state.map.getView().setCenter(userPosition);
 	}
-	
 	
 	render () {
 		return (
 			<div ref="mapContainer" style={{width: '100%', height: '100%', position:'fixed'}}></div>
 			);
 		}
-
+		
 	}
-	
-	
 	
 	export default MapEmbed;
